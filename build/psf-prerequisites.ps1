2339349f1fd39d3abc2c840fd883f7f6abbf3190
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2025-11-13 13:13:36 +0100

    improved pipeline speed

---
+++ build/psf-prerequisites.ps1
@@ -0,0 +1,23 @@
+﻿param (
+	[string]
+	$Repository = 'PSGallery'
+)
+
+Invoke-WebRequest 'https://raw.githubusercontent.com/PowershellFrameworkCollective/PSFramework.NuGet/refs/heads/master/bootstrap.ps1' | Invoke-Expression
+
+$modules = @("Pester", "PSFramework", "PSModuleDevelopment", "PSScriptAnalyzer")
+
+# Automatically add missing dependencies
+$data = Import-PowerShellDataFile -Path "$PSScriptRoot\..\PkiExtension\PkiExtension.psd1"
+foreach ($dependency in $data.RequiredModules) {
+	if ($dependency -is [string]) {
+		if ($modules -contains $dependency) { continue }
+		$modules += $dependency
+	}
+	else {
+		if ($modules -contains $dependency.ModuleName) { continue }
+		$modules += $dependency.ModuleName
+	}
+}
+
+Install-PSFModule $modules
